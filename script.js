4ad02a897dc311909bcb5041cf9ed11e30e37280
Arakka <arakka@mail.ru>
2020-01-21 15:41:18 +0300

    first

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(number);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[0]);
+
+// alert("Hello!");
+
+let answer = prompt("Are you 18?", "yes");
+console.log(typeof(arr));
