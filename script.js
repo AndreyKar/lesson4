18969d36c4ba1b8bdbb1278322b445ccb37ee84c
AndreyKar <arakka@mail.ru>
2020-01-23 22:20:41 +0300

    Lesson 3 task, version 1

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 'use strict';
 
-let money = +prompt("Ваш бюджет на месяц?","100000");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
+let money, time;
 
-// console.log(money,time);
+function start() {
+    money = +prompt("Ваш бюджет на месяц?","");
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+    while(isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?","");
+    }
+}
+start ();
 
 let appData = {
     budget: money,
@@ -11,59 +18,47 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
 
-/* Цикл с while и проверками введенных данных
-let i = 1;
-while (i <= 2) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-        b = prompt("Во сколько обойдется?", "");
-
+function chooseExpenses () {
+    for (let i=0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            b = prompt("Во сколько обойдется?", "");
+    
     if (typeof(a) === "string" && typeof(a) != null && a != '' 
         && a.length < 50 && typeof(b) === "string" && typeof(b) != null 
         && b != '' && b.length < 50) {
             console.log("Правильно введены данные");
             appData.expenses[a] = b;
-            i++;
         } else {
             alert("Данные введены неправильно! Введите верные данные");
+            i = i - 1;
         }
-} */
-
-/* Пример цилка с do с проверкой введенных данных
-let i = 1;
-do {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-        b = prompt("Во сколько обойдется?", "");
-
-    if (typeof(a) === "string" && typeof(a) != null && a != '' 
-        && a.length < 50 && typeof(b) === "string" && typeof(b) != null 
-        && b != '' && b.length < 50) {
-            console.log("Правильно введены данные");
-            appData.expenses[a] = b;
-            i++;
-        } else {
-            alert("Данные введены неправильно! Введите верные данные");
-        }
-} while (i<=2);  */
-
-for (let i=1; i <= 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    b = prompt("Во сколько обойдется?", "");
-
-if (typeof(a) === "string" && typeof(a) != null && a != '' 
-    && a.length < 50 && typeof(b) === "string" && typeof(b) != null 
-    && b != '' && b.length < 50) {
-        console.log("Правильно введены данные");
-        appData.expenses[a] = b;
-    } else {
-        alert("Данные введены неправильно! Введите верные данные");
-        i--;
     }
 }
+chooseExpenses();
 
-appData.moneyPerDay = appData.budget / 30;
+appData.moneyPerDay = (appData.budget / 30).toFixed();
 alert("Ваш расход в день: " + appData.moneyPerDay);
 
-// console.log(appData);
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка!");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка!");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка!");
+} else {
+    console.log("Произошла ошибка");
+}
+
+function checkSavings() {
+    if(appData.savings == true) {
+        let save = +prompt("Какова сумма депозита?"),
+            percent = +prompt("Под какой процент,");
+        
+        appData.monthIncome = save/100/12*percent;
+        alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+    }
+}
+checkSavings();
